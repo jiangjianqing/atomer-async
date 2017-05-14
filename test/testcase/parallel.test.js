50a860579936e2df81b840951eb5f16f6565f52a
cz_jjq <cz_jjq@163.com>
2017-05-14 10:23:22 +0800

    upgrade

--- test/testcase/parallel.test.js
+++ test/testcase/parallel.test.js
@@ -1,9 +1,12 @@
 var assert = require('assert');
 var should = require('chai').should(); //注意：使用should()后会污染js中对象的原型对象
 var expect = require('chai').expect;
+
+//-----------导入区-----------------
 var fs = require("fs");
 
 var parallel = require("../../lib/parallel");
+//----------------------------
 
 /**
  expect(foo).to.not.equal(null);
@@ -13,17 +16,21 @@ var parallel = require("../../lib/parallel");
  **/
 
 describe('parallel test', () => {
+    var tasks = [];
+    beforeEach(function(){
+        tasks = [
+            function(){
+                fs.readFile(__dirname+"/../resources/file1", "utf-8", this.done("file1"));
+            },
+            function(){
+                fs.readFile(__dirname+"/../resources/file2", "utf-8", this.done("file2"));
+            }
+        ];
+    });
 
     it('parallel correct test', done => {
         //var s = new Series();
-        var tasks = [
-            function(){
-                fs.readFile(__dirname+"/../resources/file1",this.done("file1"));
-            },
-            function(){
-                fs.readFile(__dirname+"/../resources/file2",this.done("file2"));
-            }
-        ];
+
 
         parallel(tasks, function(error, results){
             should.equal(error, null);
@@ -33,9 +40,17 @@ describe('parallel test', () => {
 
     });
 
-    it.skip('parallel fail test', done => {
+    it('parallel fail test', done => {
 
-        done();
+        tasks.push(function(){
+            fs.readFile(__dirname+"/../resources/notexistfile", "utf-8", this.done("notexistfile"));
+        });
+
+        parallel(tasks, function(error, results){
+            should.exist(error);
+            console.log(error);
+            done();
+        });
 
 
     });
