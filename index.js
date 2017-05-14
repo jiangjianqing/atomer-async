1e7342cdbd61f869011cd708e8c98c40c529243e
cz_jjq <cz_jjq@163.com>
2017-05-14 12:56:55 +0800

    upgrade

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
  * Created by cz_jjq on 17-5-14.
  */
 module.exports = {
-    "parallel" : require("lib/parallel")
+    "parallel" : require("lib/parallel"),
+    "series" : require("lib/series")
 };
