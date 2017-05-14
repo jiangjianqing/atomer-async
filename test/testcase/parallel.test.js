6e4fbcb199e966539aaa4eb30daf2fd84794f957
cz_jjq <cz_jjq@163.com>
2017-05-14 09:24:51 +0800

    upgrade

--- test/testcase/parallel.test.js
+++ test/testcase/parallel.test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var should = require('chai').should(); //注意：使用should()后会污染js中对象的原型对象
 var expect = require('chai').expect;
+var fs = require("fs");
 
 var parallel = require("../../lib/parallel");
 
@@ -15,15 +16,24 @@ describe('parallel test', () => {
 
     it('parallel correct test', done => {
         //var s = new Series();
+        var tasks = [
+            function(){
+                fs.readFile(__dirname+"/../resources/file1",this.done("file1"));
+            },
+            function(){
+                fs.readFile(__dirname+"/../resources/file2",this.done("file2"));
+            }
+        ];
 
-        done();
-
-
-
+        parallel(tasks, function(error, results){
+            should.equal(error, null);
+            console.log(results);
+            done();
+        });
 
     });
 
-    it('parallel fail test', done => {
+    it.skip('parallel fail test', done => {
 
         done();
 
